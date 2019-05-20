673c7e8160e753dafbe2ef39b00ea1291749c193
TheLonestar1 <43960228+TheLonestar1@users.noreply.github.com>
2019-05-20 14:46:21 +0700

    Add files via upload

--- dex.c
+++ dex.c
@@ -3,40 +3,112 @@
 #include<stdlib.h>
 #include<time.h>
 
+#define SIZE 6
 
 int dex_alg(int matrix[100][100], int a, int b, int *d, int prevs) {
 
-	for(int i = 0; i < 20; i++)
-		d[i] = matrix[i][a];
-	for(int i = 0; i < 20; i++) {
-		if(d[i] + prevs < d[b]){
-			
-		} 	
-	}
+	
 
 }
 
 
 
 int main(){
-
+int d[SIZE]; // ??????????? ??????????
+  int v[SIZE]; // ?????????? ???????
+  int temp;
+system("chcp 1251");
+  int minindex, min;
 	//int d[100];
-	int matrix_connect[20][20];
+	int a[SIZE][SIZE] ={0};
 	//int matrix_resh[100][100];
-
-	for(int i = 0; i < 20; i++){
-		for(int j = 0 ; j < 20; j++){
-			matrix_connect[i][j] = rand() % 50;
+	for(int i = 0; i < SIZE; i++){
+		
+		for(int j = 0 ; j < i; j++){
+			a[i][j] = (rand() % 9)+ 1 ;
 		}
-		matrix_connect[i][i] = 0;
+		a[i][i] = 0;
 	}
-	for(int i = 0; i < 20; i++){
-		for(int j =0 ; j < 20; j++){
-			printf(" %d ",matrix_connect[i][j]);
+	for(int i = 0; i < SIZE; i++){
+		
+		for(int j = 0 ; j < i; j++){
+			a[j][i] = a[i][j];
+		}
+	}
+	for(int i = 0; i < SIZE; i++){
+		for(int j =0 ; j < SIZE; j++){
+			printf(" %d ",a[i][j]);
 		}
 		printf("\n");
 	}
+	for (int i = 0; i<SIZE; i++)
+  {
+    d[i] = 10000;
+    v[i] = 1;
+  }
+  d[0] = 0;
+  // Шаг алгоритма
+  do {
+    minindex = 10000;
+    min = 10000;
+    for (int i = 0; i<SIZE; i++)
+    { // Если вершину ещё не обошли и вес меньше min
+      if ((v[i] == 1) && (d[i]<min))
+      { // Переприсваиваем значения
+        min = d[i];
+        minindex = i;
+      }
+    }
+    // Добавляем найденный минимальный вес
+    // к текущему весу вершины
+    // и сравниваем с текущим минимальным весом вершины
+    if (minindex != 10000)
+    {
+      for (int i = 0; i<SIZE; i++)
+      {
+        if (a[minindex][i] > 0)
+        {
+          temp = min + a[minindex][i];
+          if (temp < d[i])
+          {
+            d[i] = temp;
+          }
+        }
+      }
+      v[minindex] = 0;
+    }
+  } while (minindex < 10000);
+  // Вывод кратчайших расстояний до вершин
+  printf("\nКратчайшие расстояния до вершин: \n");
+  for (int i = 0; i<SIZE; i++)
+    printf("%5d ", d[i]);
 
+  // Восстановление пути
+  int ver[SIZE]; // массив посещенных вершин
+  int end = 4; // индекс конечной вершины = 5 - 1
+  ver[0] = end + 1; // начальный элемент - конечная вершина
+  int k = 1; // индекс предыдущей вершины
+  int weight = d[end]; // вес конечной вершины
+
+  while (end > 0) // пока не дошли до начальной вершины
+  {
+    for(int i=0; i<SIZE; i++) // просматриваем все вершины
+      if (a[end][i] != 0)   // если связь есть
+      {
+        int temp = weight - a[end][i]; // определяем вес пути из предыдущей вершины
+        if (temp == d[i]) // если вес совпал с рассчитанным
+        {                 // значит из этой вершины и был переход
+          weight = temp; // сохраняем новый вес
+          end = i;       // сохраняем предыдущую вершину
+          ver[k] = i + 1; // и записываем ее в массив
+          k++;
+        }
+      }
+  }
+  printf("\nВывод кратчайшего пути\n");
+  for (int i = k-1; i>=0; i--)
+    printf("%3d ", ver[i]);
+  
 	return 0;
 
 }
---
+++ wtime.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+double wtime() {
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec+(double)t.tv_usec*1E-6;
+}
